9b66bb3d188040d8f3f77c4d42036673ef8dbc87
Vladimir <zhukov1311@mail.ru>
2017-08-09 23:23:06 +0300

    Add Draw ellipse, rectangle

--- graphics editor/MainWindow.xaml.cs
+++ graphics editor/MainWindow.xaml.cs
@@ -30,16 +30,14 @@ namespace graphics_editor
          */
         void Draw_line(object sender, RoutedEventArgs e)
         {
-            Line MyLine = new Line();
-            MyLine.X1 = 1;// BeginPoint.X;
-            MyLine.Y1 = 1;// BeginPoint.Y;
-            MyLine.X2 = 35;// EndPoint.X;
-            MyLine.Y2 = 50;// EndPoint.Y;
-            MyLine.Stroke = System.Windows.Media.Brushes.Black;
-            MyLine.StrokeThickness = 1;
-            /*MyLine.HorizontalAlignment = HorizontalAlignment.Left;
-            MyLine.VerticalAlignment = VerticalAlignment.Center;*/
-            t.Children.Add(MyLine);
+            LineGeometry MyLine = new LineGeometry();
+            MyLine.StartPoint = new Point(1, 1);
+            MyLine.EndPoint = new Point(50, 40);
+            Path myPath = new Path();
+            myPath.Stroke = Brushes.Black;
+            myPath.StrokeThickness = 1;
+            myPath.Data = MyLine;
+            t.Children.Add(myPath);
         }
 
         /*
@@ -47,6 +45,49 @@ namespace graphics_editor
          */
         void Draw_rectangle(object sender, RoutedEventArgs e)
         {
+            RectangleGeometry rectangle = new RectangleGeometry();
+            rectangle.Rect = new Rect(1, 50, 60, 40);
+            Path myPath = new Path();
+            myPath.Fill = Brushes.LemonChiffon;
+            myPath.Stroke = Brushes.Red;
+            myPath.StrokeThickness = 2;
+            myPath.Data = rectangle;
+            t.Children.Add(myPath);
+        }
+
+        /*
+         * Отрисовка круга
+         */
+        void Draw_elipse(object sender, RoutedEventArgs e)
+        {
+            Path myPath = new Path();
+            myPath.Stroke = Brushes.Green;
+            //myPath.Fill = Brushes.MediumSlateBlue;
+            myPath.StrokeThickness = 3;
+            EllipseGeometry myEllipseGeometry = new EllipseGeometry();
+            myEllipseGeometry.Center = new Point(44, 140);
+            myEllipseGeometry.RadiusX = 40;
+            myEllipseGeometry.RadiusY = 40;
+            myPath.Data = myEllipseGeometry;
+            t.Children.Add(myPath);
+        }
+
+
+        /*
+         * Отрисовка неизвестного
+         */
+        void Draw_dont_know(object sender, RoutedEventArgs e)
+        {
+            Path myPath = new Path();
+            myPath.Stroke = Brushes.Black;
+            //myPath.Fill = Brushes.MediumSlateBlue;
+            myPath.StrokeThickness = 3;
+            EllipseGeometry myEllipseGeometry = new EllipseGeometry();
+            myEllipseGeometry.Center = new Point(44, 120);
+            myEllipseGeometry.RadiusX = 40;
+            myEllipseGeometry.RadiusY = 40;
+            myPath.Data = myEllipseGeometry;
+            t.Children.Add(myPath);
         }
     }
 }
